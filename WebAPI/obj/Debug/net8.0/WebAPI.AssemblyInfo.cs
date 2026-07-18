60e8bd3f68f3e546d5951f5023efaff1c2965a71
Santino Bertotti <bertottisantino42@gmail.com>
2026-07-18 13:09:33 -0300

    se agrego DelivetyDTO, DeliveryCriteriaDTO y DeliveriEndPoints

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c669201c545322889bb4ad38ce2bd47977513e7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb52b58343b0f72e2a6aa2b25e9b6f70f3d8d042")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
