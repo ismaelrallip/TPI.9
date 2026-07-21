98201782194fed30e76b4733f0d07e2fd4f92eda
Marco Tessore <marcoeltrebol@gmail.com>
2026-07-20 21:26:41 -0300

    Alta ClienteDTO

--- DTOs/ClienteDTO.cs
+++ DTOs/ClienteDTO.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.RegularExpressions;
-
-namespace DTOs
+﻿namespace DTOs
 {
     public class ClienteDTO
     {
@@ -9,9 +7,7 @@ namespace DTOs
         public string Apellido { get; set; }
 
         public string Email { get; set; }
-        public int PaisId { get; set; }
-        public string? PaisNombre { get; set; }
-        public DateTime FechaAlta { get; set; }
-
+        public string Telefono { get; set; }
+        public string Password { get; set; }
     }
 }
