9cdb6a1c07213aec02c0716bdbd95a983439870c
Santino Bertotti <bertottisantino42@gmail.com>
2026-07-23 15:19:14 -0300

    creada ejecucion con swagger

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,2 +1,36 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using WebAPI;
+using Application.Services;
+using Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Dependency Injection
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IClienteService, ClienteService>();
+
+builder.Services.AddScoped<IDeliveryRepository, DeliveryRepository>();
+builder.Services.AddScoped<IDeliveryService, DeliveryService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
+// Map endpoints
+app.MapClienteEndpoints();
+app.MapDeliveryEndpoints();
+
+app.Run();
